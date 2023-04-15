67d09ca333ad06a6f479fe6815a9062c0fb584c0
Celal Tokmak <126671343+celaltokmak61@users.noreply.github.com>
2023-04-15 04:34:30 +0300

    15/04/2023 | 04:34

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('services/', views.services, name='services'),
+    path('pricing/', views.pricing, name='pricing'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
